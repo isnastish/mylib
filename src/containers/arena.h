92663983ada2a41640c27df476d5742d1428fae5
ayevtushenko <alexey.yevtushenko@ubisoft.com>
2024-04-30 21:26:02 +0200

    Experimenting with memory chunks for arena

--- src/containers/arena.h
+++ src/containers/arena.h
@@ -2,12 +2,15 @@
 #include <cstdint>
 #include <cstring>
 #include <iterator>
+#include <list>
+#include <mutex>
 
 namespace ml
 {
 
 constexpr static std::uint64_t ALLOC_SIZE{4*1024*1024};
 
+// TODO: Remove the alignment (make it optional)
 class MemoryArena {
 public:
     MemoryArena(std::uint64_t size=ALLOC_SIZE, std::uint64_t align=4);
@@ -19,37 +22,38 @@ public:
     MemoryArena& operator=(MemoryArena&& rhs);
 
     // private?
-    template<class Object>
-    uint64_t getObjSize(const Object& obj) const {
-        uint64_t obj_size = sizeof(obj);
-        uint64_t aligned_size = obj_size + (m_align - (obj_size % m_align));
-        return aligned_size;
-    }
+    // template<class Object>
+    // uint64_t getObjSize(const Object& obj) const {
+    //     uint64_t obj_size = sizeof(obj);
+    //     // uint64_t aligned_size = obj_size + (m_align - (obj_size % m_align));
+    //     return aligned_size;
+    // }
 
     template<class Object>
     void push(const Object& obj) {
-        uint64_t obj_aligned_size = getObjSize(obj);
-        realloc(obj_aligned_size);
+        // uint64_t obj_aligned_size = getObjSize(obj);
+        uint64_t obj_size = sizeof(obj);
+        realloc(obj_size);
         *(Object *)(m_ptr + m_pos) = obj; // Or use std::memcpy
-        m_pos += obj_aligned_size;
+        m_pos += obj_size;
     }
 
     template<class Object>
     void push(Object&& obj) {
-        uint64_t obj_aligned_size = getObjSize(obj);
-        realloc(obj_aligned_size);
-        *(Object *)(m_ptr + m_pos) = std::move(obj);
-        m_pos += obj_aligned_size;
+        // uint64_t obj_aligned_size = getObjSize(obj);
+        uint64_t obj_size = sizeof(obj);
+        realloc(obj_size);
+        *reinterpret_cast<Object *>(m_ptr + m_pos) = std::move(obj); 
+        m_pos += obj_size;
     }
 
     template<class Object>
     void pop() {
-        uint64_t obj_aligned_size = sizeof(Object);
-        m_pos -= obj_aligned_size;
-        std::memset((m_ptr + m_pos), 0, obj_aligned_size);
+        uint64_t obj_size = sizeof(Object);
+        m_pos -= obj_size;
+        std::memset((m_ptr + m_pos), 0, obj_size);
     }
 
-private:
     void realloc(uint64_t size);
 
     uint8_t *m_ptr;
@@ -58,11 +62,49 @@ private:
     uint64_t m_cap;
 };
 
-// class TempMemoryArena {
-// public:
-// private:
-//     MemoryArena *m_arena;
-//     uint8_t *m_start;
-// };
+// Memory chunk
+struct Chunk {
+    uint8_t *start;
+    uint64_t size;
+    uint64_t pos;
+
+    bool is_free() { 
+        return pos == 0;
+    }
+};
+
+struct ArenaList {
+    template<class Object>
+    Chunk *push(Chunk *chunk, const Object& obj, uint64_t obj_size) {
+        // std::lock_guard<std::mutex> lock(mutex);
+        if ((chunk->pos + obj_size) <= chunk->size) {
+            *(Object *)(chunk->start + chunk->pos) = obj;
+            chunk->pos += obj_size;
+            return chunk;
+        } else {
+            for (auto itr = chunks.begin(); itr != chunks.end(); itr++) {
+                if ((itr->size <= (chunk->pos + obj_size)) && itr->is_free()) {
+                    // 1. Copy the data from the current chunk to a new one. 
+                    Chunk *new_chunk = *itr;
+                    std::memcpy(new_chunk->start, chunk->start, chunk->pos);
+                    new_chunk->pos = chunk->pos;
+                    *(Object *)(new_chunk->start + new_chunk->pos) = obj;
+                    new_chunk->pos += obj_size;
+                    // 2. Clear the current chunk so it can be reused later.
+                    std::memset(chunk->start, 0, chunk->pos);
+                    // 3. Return new chunk
+                    return new_chunk;
+                }
+            }
+            // Couldn't find an empty chunk, grow the arena, 
+            // which would mean that all the chunks would have to be recomputed.
+            // But we grow an arena by the same size.
+        }
+    }
+
+    MemoryArena *arena;
+    std::list<Chunk> chunks;
+    std::mutex mutex;
+};
 
 } // namespace ml
--- test/test_arena.cpp
+++ test/test_arena.cpp
@@ -1,42 +1,73 @@
 #include <containers/growing_array.h>
 #include <containers/arena.h>
 #include <gtest/gtest.h>
+#include <iostream>
 
-namespace test 
-{
+#define Print(arr) _print(#arr, arr)
 
 template<class T>
 class GrowingArray {
 public:
-    class const_iterator;
-    class iterator;
+    using iterator = T*;
+    using const_iterator = const T*;
 
     GrowingArray(ml::MemoryArena *arena)
-    : m_arena(arena) {}
+    : m_arena(arena), 
+    m_data(reinterpret_cast<T*>(m_arena->m_ptr + m_arena->m_pos)), 
+    m_size(0) {
+        
+    }
 
     void push_back(const T& obj) {
         m_arena->push(obj);
+        m_size++;
     }
 
     void push_back(T&& obj) {
         m_arena->push(std::move(obj));
+        m_size++;
     }
 
     void pop_back() const {
         m_arena->pop();
+        m_size--;
     }
 
+    iterator begin() { return &m_data[0]; }
+    iterator end() { return &m_data[m_size]; }
+
+    const_iterator begin() const { return &m_data[0]; }
+    const_iterator end() const { return &m_data[m_size]; }
+
 private:
-    ml::MemoryArena *m_arena;
+    ml::ArenaList *m_arena_list;
+    ml::MemoryArena *m_arena; // don't need this
+    T *m_data;
+    size_t m_size;
 };
 
-} // namespace test
+template<class T> 
+static void _print(const char *name, const GrowingArray<T>& arr) {
+    std::cout << name << ": {";
+    for (auto itr = arr.begin(); itr != arr.end(); itr++) {
+        if (itr == arr.end() - 1) {
+            std::cout << *itr;
+            break;
+        }
+        std::cout << *itr << ", ";
+    }
+    std::cout << "};" << std::endl;
+}
 
 TEST(Arena, Creation) {
     ml::MemoryArena arena;
-    test::GrowingArray<int32_t> arr(&arena);
+    GrowingArray<int32_t> arr(&arena);
 
     arr.push_back(2344);
     arr.push_back(344);
     arr.push_back(-2344);
+
+    int32_t first = *arr.begin();
+    // ASSERT_EQ(first, 2344);
+    Print(arr);
 }
